63eb64818d6764e4d5820a95b737e0de4b3308ec
Kassiane Façanha <kassianelf@alu.ufc.br>
2022-04-11 12:05:21 -0300

    atualizando todo

--- live/api-to-do/main.js
+++ live/api-to-do/main.js
@@ -1,12 +1,10 @@
-import express from 'express';
+var express = require('express');
 var app = express();
 
 // ENTIDADE (usuario, tarefas)
-
 app.get('/usuario', function(req, res) {
   res.send('Rota ativada com GET e recurso usuario: valores de usuario devem ser retornados')
 })
-
 app.get('/tarefas', function(req, res) {
   res.send('Rota ativada com GET e recurso tarefas: valores de tarefas devem ser retornados')
 })
